4a2376c153d2a67a255e6aabdc04f769996b2ddf
Phanindra Dogiparti <phanindravdogiparth@gmail.com>
2026-05-07 13:44:05 +0530

    updated code

--- config.py
+++ config.py
@@ -1,56 +1,26 @@
-# import os
-# from groq import Groq
-# import gspread
-# from oauth2client.service_account import ServiceAccountCredentials
-
-# # ✅ Groq setup
-# client = Groq(api_key=os.getenv("GROQ_API_KEY"))
-
-# # 🎤 Audio settings
-# SAMPLE_RATE = 16000
-# CHANNELS = 1
-# SILENCE_LIMIT = 5
-# CSV_FILE = "groq_transcripts.csv"
-
-# # 🔹 Google Sheets setup
-# scope = ["https://spreadsheets.google.com/feeds","https://www.googleapis.com/auth/drive"]
-# creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
-# client_gs = gspread.authorize(creds)
-# sheet = client_gs.open("AI Sales Call Assistant").sheet1
-
-
-
-
-
-import streamlit as st
+import os
 from groq import Groq
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-# Groq setup
-client = Groq(api_key=st.secrets["GROQ_API_KEY"])
+# ✅ Groq setup
+client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 
-# Audio settings
+# 🎤 Audio settings
 SAMPLE_RATE = 16000
 CHANNELS = 1
 SILENCE_LIMIT = 5
 CSV_FILE = "groq_transcripts.csv"
 
-# Google Sheets setup
-scope = [
-    "https://spreadsheets.google.com/feeds",
-    "https://www.googleapis.com/auth/drive"
-]
-
-# FIX
-creds_dict = st.secrets["GOOGLE_CREDENTIALS"]
-
-creds = ServiceAccountCredentials.from_json_keyfile_dict(
-    creds_dict,
-    scope
-)
-
+# 🔹 Google Sheets setup
+scope = ["https://spreadsheets.google.com/feeds","https://www.googleapis.com/auth/drive"]
+creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
 client_gs = gspread.authorize(creds)
-
 sheet = client_gs.open("AI Sales Call Assistant").sheet1
+
+
+
+
+
+
 
